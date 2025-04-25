6d38af92b5a8e3d4b2e23cefae9161fb361202b8
William Zeng <61915438+willzeng274@users.noreply.github.com>
2025-04-25 02:43:23 -0400

    tried some AI editors, did not like them

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,7 +3,7 @@
 --
 -- See the kickstart.nvim README for more information
 
-local sound_dir = '/Users/user/.config/nvim/lua/custom/plugins/sounds/'
+local sound_dir = vim.fn.stdpath 'config' .. '/lua/custom/plugins/sounds/'
 
 return {
   {
@@ -67,7 +67,6 @@ return {
       'InstantStop',
       'InstantStartSession',
       'InstantJoinSession',
-      'InstantStop',
       'InstantStatus',
       'InstantFollow',
       'InstantStopFollow',
@@ -232,8 +231,152 @@ return {
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
+  -- {
+  --   'github/copilot.vim',
+  -- },
   {
-    'github/copilot.vim',
+    'zbirenbaum/copilot.lua',
+    cmd = 'Copilot',
+    event = 'InsertEnter',
+    config = function()
+      require('copilot').setup {}
+    end,
+  },
+  -- {
+  --   'yetone/avante.nvim',
+  --   event = 'VeryLazy',
+  --   version = false, -- Never set this value to "*"! Never!
+  --   opts = {
+  --     -- add any opts here
+  --     -- for example
+  --     provider = 'copilot',
+  --     auto_suggestions_provider = 'copilot',
+  --     copilot = {
+  --       endpoint = 'https://api.githubcopilot.com',
+  --       model = 'claude-3.7-sonnet',
+  --       proxy = nil, -- [protocol://]host[:port] Use this proxy
+  --       allow_insecure = false, -- Allow insecure server connections
+  --       timeout = 30000, -- Timeout in milliseconds
+  --       temperature = 0,
+  --       max_tokens = 20480,
+  --     },
+  --     behaviour = {
+  --       auto_suggestions = true,
+  --       auto_set_highlight_group = true,
+  --       auto_set_keymaps = true,
+  --       auto_apply_diff_after_generation = false,
+  --       support_paste_from_clipboard = true,
+  --       minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
+  --       enable_token_counting = true, -- Whether to enable token counting. Default to true.
+  --       enable_cursor_planning_mode = false, -- Whether to enable Cursor Planning Mode. Default to false.
+  --       enable_claude_text_editor_tool_mode = false, -- Whether to enable Claude Text Editor Tool Mode.
+  --     },
+  --     suggestion = {
+  --       debounce = 600,
+  --       throttle = 600,
+  --     },
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = 'make',
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'stevearc/dressing.nvim',
+  --     'nvim-lua/plenary.nvim',
+  --     'MunifTanjim/nui.nvim',
+  --     --- The below dependencies are optional,
+  --     'echasnovski/mini.pick', -- for file_selector provider mini.pick
+  --     'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+  --     'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+  --     'ibhagwan/fzf-lua', -- for file_selector provider fzf
+  --     'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+  --     'zbirenbaum/copilot.lua', -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       'HakonHarnes/img-clip.nvim',
+  --       event = 'VeryLazy',
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { 'markdown', 'Avante' },
+  --       },
+  --       ft = { 'markdown', 'Avante' },
+  --     },
+  --   },
+  -- },
+  {
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'echasnovski/mini.diff',
+    },
+    config = function()
+      require('codecompanion').setup {
+        adapters = {
+          copilot = function()
+            return require('codecompanion.adapters').extend('copilot', {
+              schema = {
+                model = {
+                  default = 'claude-3.7-sonnet',
+                },
+              },
+            })
+          end,
+        },
+        display = {
+          action_palette = {
+            provider = 'telescope',
+            opts = {
+              show_default_actions = true,
+              show_default_prompt_library = true,
+            },
+          },
+          chat = {
+            show_references = true,
+            show_header_separator = true,
+            -- show_settings = true, -- this doesn't allow me to select a model
+          },
+          diff = {
+            provider = 'mini_diff',
+          },
+        },
+      }
+
+      vim.keymap.set({ 'n', 'v' }, '<leader>cc', ':CodeCompanionChat Toggle<CR>', {
+        noremap = true,
+        silent = true,
+        desc = 'Toggle CodeCompanion Chat',
+      })
+
+      vim.keymap.set({ 'n', 'v' }, '<C-a>', ':CodeCompanionActions<CR>', {
+        noremap = true,
+        silent = true,
+        desc = 'Open CodeCompanion Actions',
+      })
+
+      vim.keymap.set({ 'n', 'v' }, 'ga', ':CodeCompanionChat Add<CR>', {
+        noremap = true,
+        silent = true,
+        desc = 'Add selection to CodeCompanion Chat',
+      })
+
+      require('custom.plugins.companion.spinner'):init()
+    end,
   },
   {
     'vidocqh/data-viewer.nvim',
